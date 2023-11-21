d75d7fe008ccdc4a1ba961ce8bd4aa92ea73709d
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2023-11-21 14:14:58 -0300

    fix: if have any hard constraint then return inf

--- scheduling/solver/checker.py
+++ scheduling/solver/checker.py
@@ -8,7 +8,6 @@ def init_matrix(schedule: Schedule) -> Matrix:
     return [[-1 for _ in range(schedule.n_days*schedule.n_periods)] \
             for _ in range(schedule.rooms_size)]
 
-    
 
 def check_constraints(solution: Matrix, schedule: Schedule) -> int:
     """Returns the fitness of the given solution"""
@@ -16,16 +15,20 @@ def check_constraints(solution: Matrix, schedule: Schedule) -> int:
     ## Hard Constraints ##
     score = 0
     # 1. Lectures: All lectures of a course must be scheduled, and they must be assigned to distinct periods. A violation occurs if a lecture is not scheduled.
-    score += check_lectures(solution, schedule)
+    if check_lectures(solution, schedule) != 0:
+        return int(1e9)
 
     # 2. RoomOccupancy: Two lectures cannot take place in the same room in the same period. Two lectures in the same room at the same period represent one violation. Any extra lecture in the same period and room counts as one more violation.
-    score += check_room_occupancy(solution, schedule)
+    if check_room_occupancy(solution, schedule) != 0:
+        return int(1e9)
     
     # 3. Conflicts: Lectures of courses in the same curriculum or taught by the same teacher must be all scheduled in different periods. Two conflicting lectures in the same period represent one violation. Three conflicting lectures count as 3 violations: one for each pair.
-    score += check_conflicts(solution, schedule)
+    if check_conflicts(solution, schedule) != 0:
+        return int(1e9)
 
     # 4. Availabilities: If the teacher of the course is not available to teach that course at a given period, then no lectures of the course can be scheduled at that period. Each lecture in a period unavailable for that course is one violation.
-    score += check_availabilities(solution, schedule)
+    if check_availabilities(solution, schedule) != 0:
+        return int(1e9)
 
     ## Soft Constraints ##
 
@@ -116,7 +119,7 @@ def check_conflicts(matrix_solution: Matrix, schedule: Schedule) -> int:
                             if other_course.id in curriculum.members:
                                 is_same_curriculum = True
                     if is_same_curriculum or course.teacher_id == other_course.teacher_id:
-                        score += 1                
+                        score += 1
     
     return score
 
